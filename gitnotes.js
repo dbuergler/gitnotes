29ee49e99bc2222c30f721ef0d32cb21e419a8f4
Daniel Buergler <daniel.jbuergler@gmail.com>
2021-01-21 20:19:42 -0500

    setup file structure

---
+++ gitnotes.js
@@ -0,0 +1,5 @@
+// git is a version control system used to track changes in your files. Used most noteably by developers.
+// Git commits are checksumed using SHA-1, meaning no corrupt files go unnoticed without user's knowledge. 
+// Three States of Git
+// - modified
+// 
