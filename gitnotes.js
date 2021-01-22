6dc46fe9f8ef814932c0b8c2d685e4b3ed7884b9
Daniel Buergler <daniel.jbuergler@gmail.com>
2021-01-21 20:32:59 -0500

    Added more notes

--- gitnotes.js
+++ gitnotes.js
@@ -2,4 +2,20 @@
 // Git commits are checksumed using SHA-1, meaning no corrupt files go unnoticed without user's knowledge. 
 // Three States of Git
 // - modified
-// 
+// file changed, but it has not yet been committed (ex: normal work, or saving a file)
+// - staging 
+//  files that are marked to go into the next commit snapshot (utilizing git add <file_name> or git add . to add every file in the folder)
+// -commit state
+// data is safely stored in the local database (such as vm box, docker, computer, etc.)
+
+// to start version/track control
+// git init
+// creates new project
+// your project should never be inside another project folder in order to avoid inverse tree tracking issues 
+
+// stage our files for commit
+// git add <file>
+
+// use git status to check on file staging 
+
+// to commit your files, use git commit -m "your message here"
